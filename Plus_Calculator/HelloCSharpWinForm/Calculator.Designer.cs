8a9a77a150bcad4b6c648b0f1f948b9eb517c264
강재훈 <kangjaehoon@gangjaehunuiMBP.lan>
2018-09-09 14:48:49 -0400

    plus calculator

---
+++ Plus_Calculator/HelloCSharpWinForm/Calculator.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace HelloCSharpWinForm
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumButton1 = new System.Windows.Forms.Button();
+            this.NumButton2 = new System.Windows.Forms.Button();
+            this.NumButton3 = new System.Windows.Forms.Button();
+            this.NumButton4 = new System.Windows.Forms.Button();
+            this.NumButton6 = new System.Windows.Forms.Button();
+            this.NumButton5 = new System.Windows.Forms.Button();
+            this.NumButton8 = new System.Windows.Forms.Button();
+            this.NumButton7 = new System.Windows.Forms.Button();
+            this.NumButton9 = new System.Windows.Forms.Button();
+            this.NumButton0 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NumPlus = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NumButton1
+            // 
+            this.NumButton1.Location = new System.Drawing.Point(30, 77);
+            this.NumButton1.Name = "NumButton1";
+            this.NumButton1.Size = new System.Drawing.Size(62, 45);
+            this.NumButton1.TabIndex = 0;
+            this.NumButton1.Tag = "        \r\n";
+            this.NumButton1.Text = "1";
+            this.NumButton1.UseVisualStyleBackColor = true;
+            this.NumButton1.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton2
+            // 
+            this.NumButton2.Location = new System.Drawing.Point(111, 77);
+            this.NumButton2.Name = "NumButton2";
+            this.NumButton2.Size = new System.Drawing.Size(65, 45);
+            this.NumButton2.TabIndex = 0;
+            this.NumButton2.Tag = "        \r\n";
+            this.NumButton2.Text = "2";
+            this.NumButton2.UseVisualStyleBackColor = true;
+            this.NumButton2.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton3
+            // 
+            this.NumButton3.Location = new System.Drawing.Point(193, 77);
+            this.NumButton3.Name = "NumButton3";
+            this.NumButton3.Size = new System.Drawing.Size(65, 45);
+            this.NumButton3.TabIndex = 0;
+            this.NumButton3.Tag = "        \r\n";
+            this.NumButton3.Text = "3";
+            this.NumButton3.UseVisualStyleBackColor = true;
+            this.NumButton3.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton4
+            // 
+            this.NumButton4.Location = new System.Drawing.Point(30, 128);
+            this.NumButton4.Name = "NumButton4";
+            this.NumButton4.Size = new System.Drawing.Size(62, 45);
+            this.NumButton4.TabIndex = 0;
+            this.NumButton4.Tag = "        \r\n";
+            this.NumButton4.Text = "4";
+            this.NumButton4.UseVisualStyleBackColor = true;
+            this.NumButton4.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton6
+            // 
+            this.NumButton6.Location = new System.Drawing.Point(193, 128);
+            this.NumButton6.Name = "NumButton6";
+            this.NumButton6.Size = new System.Drawing.Size(65, 45);
+            this.NumButton6.TabIndex = 0;
+            this.NumButton6.Tag = "        \r\n";
+            this.NumButton6.Text = "6";
+            this.NumButton6.UseVisualStyleBackColor = true;
+            this.NumButton6.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton5
+            // 
+            this.NumButton5.Location = new System.Drawing.Point(111, 128);
+            this.NumButton5.Name = "NumButton5";
+            this.NumButton5.Size = new System.Drawing.Size(65, 45);
+            this.NumButton5.TabIndex = 0;
+            this.NumButton5.Tag = "        \r\n";
+            this.NumButton5.Text = "5";
+            this.NumButton5.UseVisualStyleBackColor = true;
+            this.NumButton5.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton8
+            // 
+            this.NumButton8.Location = new System.Drawing.Point(111, 179);
+            this.NumButton8.Name = "NumButton8";
+            this.NumButton8.Size = new System.Drawing.Size(65, 45);
+            this.NumButton8.TabIndex = 0;
+            this.NumButton8.Tag = "        \r\n";
+            this.NumButton8.Text = "8";
+            this.NumButton8.UseVisualStyleBackColor = true;
+            this.NumButton8.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton7
+            // 
+            this.NumButton7.Location = new System.Drawing.Point(30, 179);
+            this.NumButton7.Name = "NumButton7";
+            this.NumButton7.Size = new System.Drawing.Size(62, 45);
+            this.NumButton7.TabIndex = 0;
+            this.NumButton7.Tag = "        \r\n";
+            this.NumButton7.Text = "7";
+            this.NumButton7.UseVisualStyleBackColor = true;
+            this.NumButton7.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton9
+            // 
+            this.NumButton9.Location = new System.Drawing.Point(193, 179);
+            this.NumButton9.Name = "NumButton9";
+            this.NumButton9.Size = new System.Drawing.Size(65, 45);
+            this.NumButton9.TabIndex = 0;
+            this.NumButton9.Tag = "        \r\n";
+            this.NumButton9.Text = "9";
+            this.NumButton9.UseVisualStyleBackColor = true;
+            this.NumButton9.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton0
+            // 
+            this.NumButton0.Location = new System.Drawing.Point(111, 230);
+            this.NumButton0.Name = "NumButton0";
+            this.NumButton0.Size = new System.Drawing.Size(65, 45);
+            this.NumButton0.TabIndex = 0;
+            this.NumButton0.Tag = "        \r\n";
+            this.NumButton0.Text = "0";
+            this.NumButton0.UseVisualStyleBackColor = true;
+            this.NumButton0.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Gulim", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(347, 51);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // NumPlus
+            // 
+            this.NumPlus.Location = new System.Drawing.Point(301, 179);
+            this.NumPlus.Name = "NumPlus";
+            this.NumPlus.Size = new System.Drawing.Size(58, 45);
+            this.NumPlus.TabIndex = 2;
+            this.NumPlus.Text = "+";
+            this.NumPlus.UseVisualStyleBackColor = true;
+            this.NumPlus.Click += new System.EventHandler(this.NumPlus_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 282);
+            this.Controls.Add(this.NumPlus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NumButton9);
+            this.Controls.Add(this.NumButton6);
+            this.Controls.Add(this.NumButton3);
+            this.Controls.Add(this.NumButton7);
+            this.Controls.Add(this.NumButton0);
+            this.Controls.Add(this.NumButton8);
+            this.Controls.Add(this.NumButton4);
+            this.Controls.Add(this.NumButton5);
+            this.Controls.Add(this.NumButton2);
+            this.Controls.Add(this.NumButton1);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NumButton1;
+        private System.Windows.Forms.Button NumButton2;
+        private System.Windows.Forms.Button NumButton3;
+        private System.Windows.Forms.Button NumButton4;
+        private System.Windows.Forms.Button NumButton6;
+        private System.Windows.Forms.Button NumButton5;
+        private System.Windows.Forms.Button NumButton8;
+        private System.Windows.Forms.Button NumButton7;
+        private System.Windows.Forms.Button NumButton9;
+        private System.Windows.Forms.Button NumButton0;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button NumPlus;
+    }
+}
+
---
+++ Plus_Calculator/HelloCSharpWinForm/Calculator.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloCSharpWinForm
+{
+    public partial class Calculator : Form
+    {
+        public int Result = 0;
+        public bool isNewNum = true;
+
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+
+        public int Add(int number1, int number2)
+        {
+            int sum = number1 + number2;
+            return sum;
+        }
+
+        public float Add(float number1, float number2)
+        {
+            float sum = number1 + number2;
+            return sum;
+        }
+
+        public int Sub(int number1, int number2)
+        {
+            int sub = number1 - number2;
+            return sub;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumButton1_Click(object sender, EventArgs e)
+        {
+            /* Logic
+             * var =0
+             * 
+             * Enter num1 and store it in var1
+             * 
+             * Enter num2
+             * 
+             * Store it as var2 and sum var1 and var2 => store the result in var1
+             *
+             * Repeat ....
+             **/
+
+            Button numButton = (Button) sender;
+            SetNum(numButton.Text);
+        }
+
+        private void NumButton2_Click(object sender, EventArgs e)
+        {
+            SetNum("2");
+        }
+        
+        public void SetNum(string num)
+        {
+            if (isNewNum)
+            {
+                label1.Text = num;
+                isNewNum = false;
+            }
+            else if (label1.Text == "0")
+            {
+                label1.Text = num;
+            }
+            else
+            {
+                label1.Text = label1.Text + num;
+            }
+        }
+
+        private void NumPlus_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(label1.Text);
+
+            Result = Result + num;
+
+            label1.Text = Result.ToString();
+            isNewNum = true;
+        }
+    }
+}
